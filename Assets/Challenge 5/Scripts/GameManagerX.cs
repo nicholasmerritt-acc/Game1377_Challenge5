9ebb56dbe54eb1e1fa9adcaf6032f7b8bf61abf2
nicholasmerritt-acc <nicholas.merritt@g.austincc.edu>
2026-07-20 21:44:49 -0500

    challenge complete except bonus
    fix all errors
    tweak params a bit too to make game playable
    remove duplicate code

--- Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -1,56 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManagerX : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI gameOverText;
-    public GameObject titleScreen;
-    public Button restartButton; 
+    [Header("References")]
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI TimerText;
+    public TextMeshProUGUI GameOverText;
+    public GameObject TitleScreen;
+    public Button RestartButton; 
+    public List<GameObject> TargetPrefabs;
 
-    public List<GameObject> targetPrefabs;
+    [Header("Game State")]
+    [SerializeField] private int score;
+    public bool IsGameActive;
+    [SerializeField] private float spawnRate = 4f;
 
-    private int score;
-    private float spawnRate = 1.5f;
-    public bool isGameActive;
-
-    private float spaceBetweenSquares = 2.5f; 
-    private float minValueX = -3.75f; //  x value of the center of the left-most square
-    private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+    [Header("Positioning")]
+    [SerializeField] private static int maxSquareIndex = 4;
+    [SerializeField] private static float spaceBetweenSquares = 2.5f;
+    [SerializeField] private static float minValueX = -3.75f; //  x value of the center of the left-most square
+    [SerializeField] private static float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
-        isGameActive = true;
+        spawnRate /= difficulty;
+        IsGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
-        UpdateScore(0);
-        titleScreen.SetActive(false);
+        UpdateScore(score);
+        TitleScreen.SetActive(false);
     }
 
     // While game is active spawn a random target
     IEnumerator SpawnTarget()
     {
-        while (isGameActive)
+        while (IsGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
-            int index = Random.Range(0, targetPrefabs.Count);
 
-            if (isGameActive)
+            if (IsGameActive)
             {
-                Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
+                int index = Random.Range(0, TargetPrefabs.Count);
+                Instantiate(TargetPrefabs[index], RandomSpawnPosition(), TargetPrefabs[index].transform.rotation);
             }
             
         }
     }
 
     // Generate a random spawn position based on a random index from 0 to 3
-    Vector3 RandomSpawnPosition()
+    public static Vector3 RandomSpawnPosition()
     {
         float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
         float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
@@ -61,24 +65,24 @@ public class GameManagerX : MonoBehaviour
     }
 
     // Generates random square index from 0 to 3, which determines which square the target will appear in
-    int RandomSquareIndex()
+    public static int RandomSquareIndex()
     {
-        return Random.Range(0, 4);
+        return Random.Range(0, maxSquareIndex);
     }
 
     // Update score with value from target clicked
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        ScoreText.text = $"Score: {score}";
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
-        gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
-        isGameActive = false;
+        GameOverText.gameObject.SetActive(true);
+        RestartButton.gameObject.SetActive(true);
+        IsGameActive = false;
     }
 
     // Restart game by reloading the scene
--- Assets/Challenge 5/Scripts/TargetX.cs
+++ Assets/Challenge 5/Scripts/TargetX.cs
@@ -1,60 +1,32 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetX : MonoBehaviour
 {
-    private Rigidbody rb;
     private GameManagerX gameManagerX;
-    public int pointValue;
-    public GameObject explosionFx;
+    public int PointValue;
+    public GameObject ExplosionFx;
 
-    public float timeOnScreen = 1.0f;
-
-    private float minValueX = -3.75f; // the x value of the center of the left-most square
-    private float minValueY = -3.75f; // the y value of the center of the bottom-most square
-    private float spaceBetweenSquares = 2.5f; // the distance between the centers of squares on the game board
-    
+    public float TimeOnScreen = 1.0f;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
 
-        transform.position = RandomSpawnPosition(); 
+        transform.position = GameManagerX.RandomSpawnPosition(); 
         StartCoroutine(RemoveObjectRoutine()); // begin timer before target leaves screen
-
     }
 
-    // When target is clicked, destroy it, update score, and generate explosion
-    private void OnMouseEnter()
+    private void OnMouseDown()
     {
-        if (gameManagerX.isGameActive)
+        // When target is clicked, destroy it, update score, and generate explosion
+        if (gameManagerX.IsGameActive)
         {
             Destroy(gameObject);
-            gameManagerX.UpdateScore(pointValue);
+            gameManagerX.UpdateScore(PointValue);
             Explode();
         }
-               
     }
-
-    // Generate a random spawn position based on a random index from 0 to 3
-    Vector3 RandomSpawnPosition()
-    {
-        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
-        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
-
-        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
-        return spawnPosition;
-
-    }
-
-    // Generates random square index from 0 to 3, which determines which square the target will appear in
-    int RandomSquareIndex ()
-    {
-        return Random.Range(0, 4);
-    }
-
 
     // If target that is NOT the bad object collides with sensor, trigger game over
     private void OnTriggerEnter(Collider other)
@@ -64,25 +36,23 @@ public class TargetX : MonoBehaviour
         if (other.gameObject.CompareTag("Sensor") && !gameObject.CompareTag("Bad"))
         {
             gameManagerX.GameOver();
-        } 
-
+        }
     }
 
     // Display explosion particle at object's position
     void Explode ()
     {
-        Instantiate(explosionFx, transform.position, explosionFx.transform.rotation);
+        Instantiate(ExplosionFx, transform.position, ExplosionFx.transform.rotation);
     }
 
     // After a delay, Moves the object behind background so it collides with the Sensor object
     IEnumerator RemoveObjectRoutine ()
     {
-        yield return new WaitForSeconds(timeOnScreen);
-        if (gameManagerX.isGameActive)
+        yield return new WaitForSeconds(TimeOnScreen);
+        if (gameManagerX.IsGameActive)
         {
             transform.Translate(Vector3.forward * 5, Space.World);
         }
-
     }
 
 }
